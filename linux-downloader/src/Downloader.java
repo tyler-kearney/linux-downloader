5d9fc5c536746a9a77c84cc6bd199b5369e7a5b4
tyler-kearney <tyj376@gmail.com>
2024-08-29 11:45:19 -0500

    Created the list of download links.

--- linux-downloader/src/Downloader.java
+++ linux-downloader/src/Downloader.java
@@ -9,9 +9,19 @@ public class Downloader extends JFrame implements ActionListener {
     private JComboBox cbxDistro;
     private JButton btnDownload;
 
-    // todo create an array of the associated download URLs for each of the listed distributions.
+    // An array containing the download links for the isos, it matches the same order as the array stored in cbxDistro.
     private String[] isoURLs = {
-
+            "https://ubuntu.com/download/desktop/thank-you?version=24.04&architecture=amd64&lts=true",
+            "https://mirror.us.leaseweb.net/ubuntu-cdimage/xubuntu/releases/24.04/release/xubuntu-24.04-desktop-amd64.iso",
+            "https://cdimage.ubuntu.com/kubuntu/releases/24.04/release/kubuntu-24.04-desktop-amd64.iso",
+            "https://download.fedoraproject.org/pub/fedora/linux/releases/40/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-40-1.14.iso",
+            "https://download.fedoraproject.org/pub/fedora/linux/releases/40/Spins/x86_64/iso/Fedora-KDE-Live-x86_64-40-1.14.iso",
+            "https://download.fedoraproject.org/pub/fedora/linux/releases/40/Spins/x86_64/iso/Fedora-Xfce-Live-x86_64-40-1.14.iso",
+            "https://download.opensuse.org/tumbleweed/iso/openSUSE-Tumbleweed-NET-x86_64-Current.iso",
+            "https://mirror.clarkson.edu/linuxmint-images/stable/22/linuxmint-22-cinnamon-64bit.iso",
+            "https://mirror.clarkson.edu/linuxmint-images/stable/22/linuxmint-22-mate-64bit.iso",
+            "https://mirror.clarkson.edu/linuxmint-images/stable/22/linuxmint-22-xfce-64bit.iso",
+            "https://mirror.clarkson.edu/archlinux/iso/2024.08.01/archlinux-2024.08.01-x86_64.iso"
     };
 
     public Downloader() {
